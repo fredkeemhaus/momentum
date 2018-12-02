db30c0f86279a726db413a75b3d43810b154bc44
Kimjunyoung <wellkiim_@Kimjunyoungui-MacBook-Pro.local>
2018-12-02 16:34:30 +0900

    Saving the User Name part One

---
+++ gretting.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null){
+        // she is not
+    }else{
+        paintGreeting(currentUser);
+    }
+}
+function init(){
+    loadName();
+}
+init();
