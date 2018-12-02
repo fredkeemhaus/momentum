da56158e31cf76dd08af54e27af87d8e164c9b77
Kimjunyoung <wellkiim_@Kimjunyoungui-MacBook-Pro.local>
2018-12-02 12:36:47 +0900

    Making a JS Clock part Two

--- clock.js
+++ clock.js
@@ -6,10 +6,14 @@ function getTime(){
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerHTML = `${hours}:${minutes}:${seconds}`;
+    clockTitle.innerHTML = `${
+        hours < 10 ? `0${hours}` : hours}:${
+            minutes < 10 ? `0${minutes}` : minutes }:${
+        seconds < 10 ? `0${seconds}` : seconds}`
 }
 
 function init(){
     getTime();
+    setInterval(getTime, 1000);
 }
 init();
