5b6274e5639dde02aeaf93c02218db9a4cb1219c
Kimjunyoung <wellkiim_@Kimjunyoungui-MacBook-Pro.local>
2018-12-02 19:43:11 +0900

    Getting the Weather part One

---
+++ weather.js
@@ -0,0 +1,48 @@
+const API_KEY = "0c7c5f5a97cb936569a6f7ce3f719b77";
+const COORDS = 'coords';
+
+function getWeather(lat, lng){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric
+    `).then(function(json){
+        console.log(json);
+    })
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude,
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude)
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location")
+}
+
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError)
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    }else{
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
